70e65f59175c3940b67ea36b096c4317ac0846cf
Ma161ric <ma161ric@htwg-konstanz.de>
2023-04-27 19:39:15 +0200

    SE05: fixing main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,49 +3,64 @@ import controller.Controller
 import scala.io.StdIn.readLine
 import model.{Card, Field, FightField, Hand, Player}
 import view.Tui
+import scala.util.Random
 
 @main def run: Unit =
 
+  val rnd = new Random
+  var input = ""
 
   println("Welcome to my game!")
 
   print("Size of game (min 4): ")
-  val size = readLine().toInt // muss integer sein, sollte wert 6 haben
+  input = readLine()
+  while (input.toInt < 4 || input.toInt > 10) {
+    print("Size of game (min 4): ")
+    input = readLine()
+  }
+  val size = input.toInt
+  // muss integer sein, sollte wert 6 haben
   val emptyHand = Hand(List.fill(size)(Card.emptyCard))
   val emptyFightField = FightField(List.fill(size)(Card.emptyCard))
 
   println("Please enter your names: ")
   print("Player 1 Name: ")
   val firstName = readLine()
-  val firstPlayer = Player(firstName, false, emptyHand, emptyFightField)
 
   print("Player 2 Name: ")
-  val secondName = readLine()
-  val secondPlayer = Player(secondName, false, emptyHand, emptyFightField)
+  input = readLine()
 
-  while (firstPlayer.name == secondPlayer.name) {
+  while (firstName == input) {
     println("Player 1 and Player 2 cannot have the same name!")
     print("Player 2 Name: ")
-    val secondName = readLine()
-    val secondPlayer2 = Player(secondName, false, emptyHand, emptyFightField)
+    input = readLine()
     println()
   }
+  val secondName = input
+  val firstPlayer = Player(firstName, false, emptyHand, emptyFightField)
+  val secondPlayer = Player(secondName, false, emptyHand, emptyFightField)
 
-  val field = Field(size, 1, 40, emptyHand, emptyFightField, firstPlayer, secondPlayer)
+  val field =
+    Field(size, 1, 40, emptyHand, emptyFightField, firstPlayer, secondPlayer)
   val controller = Controller(field)
   val tui = new Tui(controller)
 
+  // print("Who starts first (1 or 2)? ") // input 1 or 2 or q
+  // input = readLine()
+  val goFirst = rnd.nextInt(2) + 1
+  if (goFirst == 1) {
+    println(firstPlayer.name + " starts first !") // input 1 or 2 or q
+  } else {
+    println(secondPlayer.name + " starts first !")
+  }
+  val tuiField: Unit =
+    tui.processInputLine(
+      size,
+      goFirst.toString(),
+      field,
+      firstPlayer,
+      secondPlayer
+    )
 
-  print("Who starts first (1 or 2)? ") // input 1 or 2 or q
-  val input = readLine()
-  val tuiField: Unit = tui.processInputLine(size, input, field, firstPlayer, secondPlayer)
-
-  //println(field.toString)
-  //println(field.mesh(size,size, input, Card.roterDrache))
-
-
-
-
-
-
-
+  // println(field.toString)
+  // println(field.mesh(size,size, input, Card.roterDrache))
